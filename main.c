6d1cb74cd3361cf7bba9957bc9c436c8bceb1598
Lauren <lpehlivanian00@stuy.edu>
2020-03-24 18:42:14 -0400

    add and draw polygon works:

--- main.c
+++ main.c
@@ -12,6 +12,9 @@
 int main(int argc, char **argv) {
 
   screen s;
+  color c;
+  clear_screen(s);
+  c.green = 255;
   struct matrix * edges;
   struct matrix * polygons;
   struct matrix * transform;
@@ -20,10 +23,15 @@ int main(int argc, char **argv) {
   polygons = new_matrix(4, 4);
   transform = new_matrix(4, 4);
 
-  if ( argc == 2 )
-    parse_file( argv[1], transform, edges, polygons, s );
-  else
-    parse_file( "stdin", transform, edges, polygons, s );
+  // if ( argc == 2 )
+  //   parse_file( argv[1], transform, edges, polygons, s );
+  // else
+  //   parse_file( "stdin", transform, edges, polygons, s );
+
+  add_polygon(edges,100.0,100.0,0.0, 150.0,150.0,0.0, 200.0,100.0,0.0);
+  print_matrix(edges);
+  draw_polygons(edges,s,c);
+  display(s);
 
   free_matrix( edges );
   free_matrix( polygons );
